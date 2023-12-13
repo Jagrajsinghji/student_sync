fb1ebb84d41d9ed04e6a780b423ddfc4ff4268b5
Jagraj Singh <jagrajsinghji99@gmail.com>
2023-12-12 19:04:05 -0500

    added notifications, ocr

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import flutter_local_notifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 10.0, *) {
+           UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey("AIzaSyA_c2a1FsszRo25CDwu8uCUMlTA3HIyOTY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
